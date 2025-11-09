3bff02024a7696b648d4428d8c6726aeb9f29061
FabBot <76592775+alwynsajan@users.noreply.github.com>
2025-11-09 22:37:27 +0800

    Added changes to have three different ToDos.

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -4,12 +4,15 @@ import NavBar from './NavBar'
 import Contents from './Contents'
 
 function Container() {
-    const [toDoList, setToDoList] = useState([]);
+    const [personaltoDoList, setpersonaltoDoList] = useState([]);
+    const [familytoDoList, setfamilytoDoList] = useState([]);
+    const [worktoDoList, setworktoDoList] = useState([]);
+    const [activeCategory, setActiveCategory] = useState('personal'); // default = personal
 
   return (
     <div className="containerElement">
-        <NavBar toDoList= {toDoList} setToDoList={setToDoList} />
-        <Contents toDoList= {toDoList} setToDoList={setToDoList}/>
+        <NavBar personaltoDoList= {personaltoDoList} setpersonaltoDoList={setpersonaltoDoList} familytoDoList={familytoDoList} worktoDoList={worktoDoList} setfamilytoDoList={setfamilytoDoList} setworktoDoList={setworktoDoList} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
+        <Contents personaltoDoList= {personaltoDoList} setpersonaltoDoList={setpersonaltoDoList} familytoDoList= {familytoDoList} worktoDoList={worktoDoList} setfamilytoDoList={setfamilytoDoList} setworktoDoList={setworktoDoList} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
     
     </div>
   )
